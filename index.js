3d6d2dd2c854d47ee2c88d50098b096274dfcbad
flaming0dev <piller.manuel@premontrei-zsambek.edu.hu>
2024-10-01 12:30:31 +0200

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+
+let win
+
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit()
+})
+
+app.on("ready", () => {
+
+
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 400,
+    minHeight: 300,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: "src/img/icon.ico"
+  });
+
+
+
+  
+  ipcMain.on("close-app", () => {win.close()});
+  ipcMain.on("minimize-app", () => {win.minimize()});
+  ipcMain.on("maximize-app", () => {
+    if (win.isMaximized()) {
+      win.restore()
+    } else {
+      win.maximize()
+    }
+  });
+
+
+  win.loadFile("index.html");
+})
